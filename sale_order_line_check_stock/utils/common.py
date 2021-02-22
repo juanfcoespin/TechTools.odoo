bcfbc0e97ddc2de1ad6691e1f69d5a53ccedabb4
juanfcoespin <juan.espin@yahoo.com>
2021-02-22 07:12:01 -0500

    21Feb2021

--- sale_order_line_check_stock/utils/common.py
+++ sale_order_line_check_stock/utils/common.py
@@ -2,6 +2,6 @@ def hay_productos_sin_stock(self):
     sin_stock = False
     for line in self:
         # si la cantidad del producto es mayor que el stock
-        if (line.product_id and line.product_uom_qty > line.product_id.qty_available):
+        if (line.product_id and line.product_id.type != 'service' and line.product_uom_qty > line.product_id.qty_available):
             sin_stock = True
     return sin_stock
